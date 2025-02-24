84ebdf38f4d7481eef2493460faf324599022681
Muhammad Ahmad <muhammadahmad338@github.com>
2025-02-24 19:47:26 +0500

    latest commits

---
+++ app/components/Logo.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const Logo: FC = () => {
+  return (
+    <div>Logo Ahmad</div>
+  )
+}
+
+export default Logo
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Code, Fira_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const firaCode = Fira_Code({ 
+    weight: ['400'],
+    variable: "--font-fira-code",
+    subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const firaMono = Fira_Mono({ 
+  weight: ['400'],
+  variable: "--font-fira-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased bg-black flex flex-col items-center justify-center`} 
       >
         {children}
       </body>
